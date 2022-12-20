89c906633fad94204128306c8d4ad5f47b8ec8b0
Daniel Easterman <daniel.easterman@gmail.com>
2022-12-20 14:08:35 +0000

    Downgrade python version to 9, to fix 3rd party collections mapping bug

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import requests
 from pprint import pprint
-from PyInquirer import prompt, Separator
+from PyInquirer import prompt, print_json, Separator
+
 
 def google_books_query():
+	user_query = [{
+		'type': 'input',
+		'name': 'keyword',
+		'message': "Enter a keyword to query the Google Books API:",
+    }]
+	user_answer = prompt(user_query)
 	# URL for the Google Books API:
 	api_url = "https://www.googleapis.com/books/v1/volumes"
 	# Query parameters for the API request:
-	params = {"q": "python", "maxResults": 5}
+	params = {"q": user_answer["keyword"], "maxResults": 5}
 	# Make a GET request to the API:
 	response = requests.get(api_url, params=params)
 	# Early abort if status code not 200:
@@ -24,5 +31,7 @@ def google_books_query():
 		books.append(book)
 	pprint(books)
 	# print(f"{title} by {author} - ({publisher})")
+
+
 
 google_books_query()
