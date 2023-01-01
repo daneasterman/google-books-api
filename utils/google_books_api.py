d9be02472482c01862b2fdba18d3b146d3ba3f81
Daniel Easterman <daniel.easterman@gmail.com>
2023-01-01 18:19:18 +0000

    Use join to convert list of authors to comma separated string of authors

--- utils/google_books_api.py
+++ utils/google_books_api.py
@@ -6,21 +6,21 @@ load_dotenv()
 
 def get_books(query):
 	GOOGLE_BOOKS_API_KEY = os.getenv('GOOGLE_BOOKS_API_KEY')
-	params = {"q": query, "maxResults": 5, "key": GOOGLE_BOOKS_API_KEY}	
+	params = {"q": f"{query}intitle:", "maxResults": 5, "key": GOOGLE_BOOKS_API_KEY}	
 	API_URL = "https://www.googleapis.com/books/v1/volumes"	
 	api = requests.get(API_URL, params=params)
 	data = api.json()
 	found_books = []
 	for book in data["items"]:
+			authors = ", ".join(book["volumeInfo"]["authors"])
 			book = {
-				"author": book["volumeInfo"]["authors"],
-				"title": book["volumeInfo"]["title"],
+				"authors": authors,
+				"name": book["volumeInfo"]["title"],
 				"publisher": book["volumeInfo"].get("publisher", "None")
 			}
 			found_books.append(book)
 	if found_books:
 		print("Your 5 books:")
 		for book in found_books:
-			print(f"Title: {book['title']}, Author: {book['author']}, Publisher: ({book['publisher']})")
-	
+			print(f"Title: {book['name']}, Authors: {book['authors']}, Publisher: ({book['publisher']})")	
 	return found_books
